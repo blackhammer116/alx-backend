03b4a05c62ce382849983352759ed36b60d9e6d1
Nebiyu <nebiyuhiyabu@gmail.com>
2023-08-29 01:35:59 -0700

    simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Flask: module to run a flask app
+render_template: used to render an html page
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    Simple route that leads to index.html
+    template.
+    """
+    return render_template('0-index.html')
